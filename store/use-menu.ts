51252aa927c86eaf396da4997b8364b74307133a
ton1czech <tonyasek007@gmail.com>
2024-02-10 22:47:14 +0100

    🎁NEW: global state menu

---
+++ store/use-menu.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+
+interface MenuStore {
+  isMenuOpen: boolean
+  openMenu: () => void
+  closeMenu: () => void
+}
+
+export const useMenu = create<MenuStore>(set => ({
+  isMenuOpen: false,
+  openMenu: () => set({ isMenuOpen: true }),
+  closeMenu: () => set({ isMenuOpen: false }),
+}))
