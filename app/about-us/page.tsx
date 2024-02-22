1c3f08341e2a30368b9249dcc9e9d026fc23d0e0
ton1czech <tonyasek007@gmail.com>
2024-02-22 10:38:09 +0100

    🎁NEW: about us dummy video

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,8 +1,10 @@
 import { Contact } from '@/components/about/contact'
+import { Intro } from '@/components/about/intro'
 
 export default function Page() {
   return (
-    <main className='pt-32 pb-24'>
+    <main className='pt-32 pb-24 space-y-20 lg:space-y-40'>
+      <Intro />
       <Contact />
     </main>
   )
---
+++ components/about/intro.tsx
@@ -0,0 +1,11 @@
+import { Container } from '../container'
+
+export const Intro = () => {
+  return (
+    <Container>
+      <video controls playsInline>
+        <source src='https://cdn.coverr.co/videos/coverr-land-rover-parked-at-the-beach-1675/1080p.mp4' />
+      </video>
+    </Container>
+  )
+}
