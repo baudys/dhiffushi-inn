dfcd0eabb881584b3db64ae28ba2b32fd41efe10
ton1czech <tonyasek007@gmail.com>
2024-02-22 10:29:10 +0100

    🐛BUG FIX: gallery row spacing

--- components/gallery/row.tsx
+++ components/gallery/row.tsx
@@ -12,9 +12,9 @@ export const Row = ({ images }: Props) => {
     <Carousel>
       {images.map((image, i) => {
         return (
-          <CarouselItem index={i} key={i}>
+          <CarouselItem index={i} key={i} className='px-2'>
             <>
-              <div className='relative aspect-video w-[78vw] lg:w-[50vw] rounded mx-2'>
+              <div className='relative aspect-video w-[78vw] lg:w-[50vw]'>
                 <img
                   src={image}
                   className='absolute inset-0 w-full h-full object-cover'
