a6993c59b207289dc734424032d9a0fb5f941411
ton1czech <tonyasek007@gmail.com>
2024-02-22 13:36:24 +0100

    🎁NEW: featured experience homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,23 @@ import Hero from '@/components/home/hero'
 import { Influencer } from '@/components/home/influencer'
 import { Reviews } from '@/components/home/reviews'
 import { cachedClient } from '@/sanity/lib/client'
-import { ReviewsQuery } from '@/sanity/lib/queries'
+import { DivingQuery, ReviewsQuery } from '@/sanity/lib/queries'
 
 export const revalidate = 60
 
 export default async function Page() {
   const reviews = await cachedClient(ReviewsQuery)
+  const diving = await cachedClient(DivingQuery)
+  let experiences: any = []
+
+  experiences = experiences.concat(diving)
 
   return (
     <main className='pb-24 space-y-40'>
       <Hero />
-      <Experiences />
+      <Experiences
+        experience={experiences[Math.floor(Math.random() * experiences.length)]}
+      />
       <Gallery />
       <Reviews reviews={reviews.slice(0, 2)} />
       <Influencer />
--- components/home/experiences.tsx
+++ components/home/experiences.tsx
@@ -1,13 +1,54 @@
+'use client'
+
+import { urlForImage } from '@/sanity/lib/image'
 import { Container } from '../container'
 import { Title } from './title'
+import { useLanguage } from '@/store/use-language'
+import { Calendar, Timer } from 'lucide-react'
+import Link from 'next/link'
 
-export const Experiences = () => {
+interface Props {
+  experience: any
+}
+
+export const Experiences = ({ experience }: Props) => {
+  const { language } = useLanguage()
+
   return (
     <Container>
       <Title
         labelCz='Zažij s námi něco nového'
         labelEn='Experience something new with us'
       />
+      <Link href={`/experiences/${experience.slug.current}`}>
+        <div className='relative aspect-video overflow-hidden grid items-end'>
+          <div className='absolute bg-gradient-to-r from-black/80 to-transparent from-30% inset-0 z-10 pointer-events-none' />
+          <img
+            src={urlForImage(experience.image)}
+            className='absolute inset-0 w-full h-full object-cover hover:scale-110 transition duration-300'
+          />
+
+          <div className='relative z-10 p-8'>
+            <h2 className='font-medium text-white text-3xl'>
+              {language === 'cz' && experience.titleCz}
+              {language === 'en' && experience.titleEn}
+            </h2>
+            <p className='text-sm text-zinc-400 flex experiences-center gap-1'>
+              <Timer size={18} />
+              {experience.duration}
+            </p>
+            <p className='text-sm text-zinc-400 flex experiences-center gap-1'>
+              <Calendar size={18} />
+              {language === 'cz' && experience.daysCz}
+              {language === 'en' && experience.daysEn}
+            </p>
+            <p className='text-white text-lg'>
+              od {language === 'cz' && experience.priceCz}
+              {language === 'en' && experience.priceEn}
+            </p>
+          </div>
+        </div>
+      </Link>
     </Container>
   )
 }
