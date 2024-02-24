ce1354895f081a71a5483bdb828228556c6347e7
ton1czech <tonyasek007@gmail.com>
2024-02-24 18:28:48 +0100

    🎁NEW: revies title

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,3 +1,5 @@
+import { Container } from '@/components/container'
+import { Title } from '@/components/home/title'
 import { ReviewsGrid } from '@/components/reviews/reviews-grid'
 import { cachedClient } from '@/sanity/lib/client'
 import { ReviewsQuery } from '@/sanity/lib/queries'
@@ -9,6 +11,12 @@ export default async function Page() {
 
   return (
     <main className='pt-32 pb-24'>
+      <Container className='mb-6 lg:mb-14'>
+        <Title
+          labelCz='Přečtěte si, co o nás říkají naši hosté'
+          labelEn='Read what our guests have to say about us'
+        />
+      </Container>
       <ReviewsGrid reviews={reviews} />
     </main>
   )
