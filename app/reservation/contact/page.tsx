a3b856410915d5b6323e5f6c1d532e585dae4989
ton1czech <danielanthonybaudys@gmail.com>
2024-03-10 21:01:29 +0500

    ❌REMOVED: summary

--- app/reservation/contact/page.tsx
+++ app/reservation/contact/page.tsx
@@ -1,13 +1,11 @@
 import { Container } from '@/components/container'
 import { Contact } from '@/components/reservation/contact'
-import { Summary } from '@/components/reservation/summary'
 
 export default function Page() {
   return (
     <main className='pt-32 pb-24'>
-      <Container className='grid md:grid-cols-[2fr_1fr] gap-4 md:gap-10'>
+      <Container className='sm:w-[70%] lg:w-[50%]'>
         <Contact />
-        <Summary />
       </Container>
     </main>
   )
--- components/reservation/summary.tsx
+++
@@ -1,7 +0,0 @@
-export const Summary = () => {
-  return (
-    <section className='order-1 md:order-2'>
-      <p>summary</p>
-    </section>
-  )
-}
