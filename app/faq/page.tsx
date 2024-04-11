3f58f6a8f7c0ca29ba61d394943d4977e7b0f809
ton1czech <tonyasek007@gmail.com>
2024-04-11 21:28:16 +0200

    👌 IMPROVE: faq gap + layout

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -16,45 +16,47 @@ export default function Page() {
 
   return (
     <main className='py-28'>
-      <Container className='grid lg:grid-cols-2 gap-4 lg:gap-32'>
+      <Container>
         <Title
           labelCz='Nejčastěji kladené otázky'
           labelEn='Frequently asked questions'
           labelRu='Часто задаваемые вопросы'
         />
 
-        <Accordion type='multiple' className='space-y-4'>
-          {faq.slice(0, 5).map(item => (
-            <AccordionItem key={item.value} value={item.value}>
-              <AccordionTrigger className='text-lg'>
-                {language === 'cz' && item.qCz}
-                {language === 'en' && item.qEn}
-                {language === 'ru' && item.qRu}
-              </AccordionTrigger>
-              <AccordionContent className='text-base'>
-                {language === 'cz' && item.aCz}
-                {language === 'en' && item.aEn}
-                {language === 'ru' && item.aRu}
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
-        <Accordion type='multiple' className='space-y-4'>
-          {faq.slice(5, 10).map(item => (
-            <AccordionItem key={item.value} value={item.value}>
-              <AccordionTrigger className='text-lg'>
-                {language === 'cz' && item.qCz}
-                {language === 'en' && item.qEn}
-                {language === 'ru' && item.qRu}
-              </AccordionTrigger>
-              <AccordionContent className='text-base'>
-                {language === 'cz' && item.aCz}
-                {language === 'en' && item.aEn}
-                {language === 'ru' && item.aRu}
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
+        <div className='grid lg:grid-cols-2 gap-4 lg:gap-20'>
+          <Accordion type='multiple' className='space-y-4'>
+            {faq.slice(0, 5).map(item => (
+              <AccordionItem key={item.value} value={item.value}>
+                <AccordionTrigger className='text-lg'>
+                  {language === 'cz' && item.qCz}
+                  {language === 'en' && item.qEn}
+                  {language === 'ru' && item.qRu}
+                </AccordionTrigger>
+                <AccordionContent className='text-base'>
+                  {language === 'cz' && item.aCz}
+                  {language === 'en' && item.aEn}
+                  {language === 'ru' && item.aRu}
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+          <Accordion type='multiple' className='space-y-4'>
+            {faq.slice(5, 10).map(item => (
+              <AccordionItem key={item.value} value={item.value}>
+                <AccordionTrigger className='text-lg'>
+                  {language === 'cz' && item.qCz}
+                  {language === 'en' && item.qEn}
+                  {language === 'ru' && item.qRu}
+                </AccordionTrigger>
+                <AccordionContent className='text-base'>
+                  {language === 'cz' && item.aCz}
+                  {language === 'en' && item.aEn}
+                  {language === 'ru' && item.aRu}
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
       </Container>
     </main>
   )
